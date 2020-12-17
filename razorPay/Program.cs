c6ef303dae48ac2cbf95437be0e440e35e0c9501
guna.ashok1991 <guna.ashok8@gmail.com>
2020-12-17 19:13:20 +0530

    #0002 program.cs url changed

--- razorPay/Program.cs
+++ razorPay/Program.cs
@@ -20,7 +20,7 @@ namespace razorPay
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseUrls(new[] { "http://localhost:5003" }); ;
                 });
     }
 }
